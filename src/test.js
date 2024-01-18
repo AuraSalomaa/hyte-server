20219f780a61eb8a9668c3c1a902b387f4682f2a
AuraSalomaa <aura@salomaa.info>
2024-01-18 10:17:20 +0200

    express lisätty yksinkertaistamiseksi

--- src/test.js
+++ src/test.js
@@ -1,3 +1,18 @@
-console.log("testi")
-console.log("testi2")
-console.log("testi3")
+import  express  from 'express';
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express();
+
+app.get('/items',(req, res)=>{
+  res.json([
+    { "id": 1, "name": "Item1" },
+    { "id": 2, "name": "Item2" }
+
+  ])
+
+});
+
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
